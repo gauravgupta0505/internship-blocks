d5475a5bba42fbbbda1f975a6b54ed9835e604dc
gauravgupta0505 <80693264+gauravgupta0505@users.noreply.github.com>
2023-05-12 11:53:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // Using hooks to manipulate the position of the block
+  // Can be visualized in a 2D-Plane, clicking buttons will change its X and Y position
+  const [xPosition, setxPosition] = useState(0);
+  const [yPosition, setyPosition] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="wrapper-container">
+        {/* The buttons are disabled using 'disabled ' attribute /*/}
+        <button
+          className="h-btn"
+          disabled={yPosition === 0}
+          onClick={() => setyPosition(yPosition - 50)}
         >
-          Learn React
-        </a>
-      </header>
+          Up
+        </button>
+
+        <div className="container">
+          <button
+            className="v-btn"
+            disabled={xPosition === 0}
+            onClick={() => setxPosition(xPosition - 50)}
+          >
+            Left
+          </button>
+          
+          <div className="main-grid">
+            <div
+              className="box"
+              style={{ left: xPosition, top: yPosition }}
+            ></div>
+          </div>
+
+          <button
+            className="v-btn"
+            disabled={xPosition === 450}
+            onClick={() => setxPosition(xPosition + 50)}
+          >
+            Right
+          </button>
+
+        </div>
+        
+        <button
+          className="h-btn"
+          disabled={yPosition === 450}
+          onClick={() => setyPosition(yPosition + 50)}
+        >
+          Bottom
+        </button>
+      </div>
     </div>
   );
 }
